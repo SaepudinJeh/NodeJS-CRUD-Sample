7e141020bbcb130c4b187e9f1ea1926d9e9754d7
SaepudinJeh <saepudinjeh@gmail.com>
2020-09-24 17:00:22 +0700

    add repo again

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import usersRouters from './routes/users.js';
+
+const app = express();
+const PORT = 5000;
+
+
+app.use(bodyParser.json());
+
+app.use('/users', usersRouters);
+
+app.get('/', (req, res)=> res.send("Hello from Homepage."));
+
+app.listen(PORT, ()=> console.log(`server on running on port: http://localhost:${PORT}`));
+
